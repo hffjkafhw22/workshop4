c3c3b862084b758851441a1c426ad9cd4d124c5a
HONGYAN <zhy19930227@gmail.com>
2019-08-19 23:21:31 +1000

    component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AccountComponent } from './account/account.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(
+      [
+        {
+         path: 'home',
+         redirectTo:'home',
+         pathMatch: 'full',
+         component: AppComponent
+        },
+
+        { path: 'account', 
+         component: AccountComponent 
+        },
+        {
+         path: 'login',
+         component: LoginComponent
+        }    
+      ]
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
